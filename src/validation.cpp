3d0e893ed46b463de669c138c07a45d4fa93de44
Markus Bingert <38150838+markusbingert@users.noreply.github.com>
2019-03-21 17:41:44 +0100

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -1,3 +1,5 @@
+// Hier passiert die ganze Validierung -> validation.cpp
+
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2018 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
